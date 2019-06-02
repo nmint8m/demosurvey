888177b31599fcbb7da41fd960a133b87f1f0a48
Tam Nguyen M <nmint8m@gmail.com>
2019-06-02 10:21:53 +0700

    Init project

---
+++ DemoSurvey/Definitions/Measurement.swift
@@ -0,0 +1,11 @@
+//
+//  Measurement.swift
+//  DemoSurvey
+//
+//  Created by Tam Nguyen M. on 6/2/19.
+//  Copyright © 2019 Tam Nguyen M. All rights reserved.
+//
+
+import UIKit
+
+let screenSize = UIScreen.main.bounds.size
---
+++ DemoSurvey/ViewControllers/SurveyList/SurveyListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SurveyListViewController.swift
+//  DemoSurvey
+//
+//  Created by Tam Nguyen M. on 6/2/19.
+//  Copyright © 2019 Tam Nguyen M. All rights reserved.
+//
+
+import UIKit
+
+final class SurveyListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
