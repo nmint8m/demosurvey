32bde9dce0f205cb24ed6f6039cdaaabdc74d1cd
Tam Nguyen M <nmint8m@gmail.com>
2019-06-03 17:28:31 +0700

    Modify unit test

--- DemoSurveyTests/DemoSurveyTests.swift
+++ DemoSurveyTests/DemoSurveyTests.swift
@@ -22,7 +22,6 @@ class DemoSurveyTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        XCTFail("Test error")
     }
 
     func testPerformanceExample() {
