9dde96e7551984b9530b78a360de7ceb9cb2b574
Tam Nguyen M <nmint8m@gmail.com>
2019-06-05 22:30:00 +0700

    Write unit test for core services

---
+++ DemoSurveyTests/Definition.swift
@@ -0,0 +1,11 @@
+//
+//  Definition.swift
+//  DemoSurveyTests
+//
+//  Created by Tam Nguyen M. on 6/5/19.
+//  Copyright © 2019 Tam Nguyen M. All rights reserved.
+//
+
+import Foundation
+
+let host = "nimble-survey-api.herokuapp.com"
