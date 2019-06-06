4a84ec5551ad4073a205f9d5ff957ebd0e84c694
Tam Nguyen M <nmint8m@gmail.com>
2019-06-06 21:47:09 +0700

    Unit test for AuthenticationServiceTests

--- DemoSurveyTests/API/AuthenticationServiceTests/AuthenticationServiceTests.swift
+++ DemoSurveyTests/API/AuthenticationServiceTests/AuthenticationServiceTests.swift
@@ -15,8 +15,72 @@ import OHHTTPStubs
 final class AuthenticationServiceTests: QuickSpec {
 
     override func spec() {
-        it("response should be succeed and have detectItem") {
-            expect("abc") == "abc"
+
+        beforeEach {
+            Session.shared.setValue("", for: .accessToken)
         }
+
+        describe("Test AuthenticationService") {
+
+            it("Test getAccessToken success") {
+                stub(condition: isHost(host) && isPath(Config.authenPath)) { _ -> OHHTTPStubsResponse in
+                    guard let path = OHPathForFile(Config.successFilePath, type(of: self)) else {
+                        fail("File not found")
+                        return OHHTTPStubsResponse(error: Test.Error.fileNotFound)
+                    }
+                    return fixture(filePath: path, status: 200, headers: nil)
+                        .setRequestTime()
+                }
+
+                waitUntil(timeout: 10) { done in
+                    API.Authentication.getAccessToken() { result in
+                        switch result {
+                        case .success:
+                            expect(Session.shared.getValue(for: .accessToken)) == "Bearer f8931eaf2fe071e40c0b594635edae607cc2ac41d84e12cac3a2b93c377f29da"
+                        case .failure:
+                            fail("Should return success")
+                        }
+                        done()
+                    }
+                }
+            }
+
+            it("Test getAccessToken failure") {
+                stub(condition: isHost(host) && isPath(Config.authenPath)) { _ -> OHHTTPStubsResponse in
+                    guard let path = OHPathForFile(Config.errorFilePath, type(of: self)) else {
+                        fail("File not found")
+                        return OHHTTPStubsResponse(error: Test.Error.fileNotFound)
+                    }
+                    return fixture(filePath: path, status: 401, headers: nil)
+                        .setRequestTime()
+                }
+
+                waitUntil(timeout: 10) { done in
+                    API.Authentication.getAccessToken() { result in
+                        switch result {
+                        case .success:
+                            fail("Should return error")
+                        case .failure(let error):
+                           expect((error as NSError).code) == API.Error.unauthorized.code
+                           expect(Session.shared.getValue(for: .accessToken)).to(beNil())
+                        }
+                        done()
+                    }
+                }
+            }
+        }
+
+        afterEach {
+            OHHTTPStubs.removeAllStubs()
+        }
+    }
+}
+
+extension AuthenticationServiceTests {
+
+    private struct Config {
+        static let authenPath = "/oauth/token"
+        static let successFilePath = "AuthenticationTests-Success.json"
+        static let errorFilePath = "AuthenticationTests-401Error.json"
     }
 }
